20b560eedd76b41421d228cd4b2a217e1c297f02
Jaime van Kessel <nallath@gmail.com>
2019-07-30 14:20:58 +0200

    Update intent models to also house nested qualities
    CURA-6598

--- cura/Machines/Models/IntentModel.py
+++ cura/Machines/Models/IntentModel.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import Qt, QObject, pyqtProperty, pyqtSignal
 
 from UM.Qt.ListModel import ListModel
 from UM.Settings.ContainerRegistry import ContainerRegistry
+from UM.Settings.SettingFunction import SettingFunction
 
 from cura.Settings.IntentManager import IntentManager
 import cura.CuraApplication
@@ -15,18 +16,22 @@ import cura.CuraApplication
 class IntentModel(ListModel):
     NameRole = Qt.UserRole + 1
     QualityTypeRole = Qt.UserRole + 2
+    LayerHeightRole = Qt.UserRole + 3
+    AvailableRole = Qt.UserRole + 4
 
     def __init__(self, parent: Optional[QObject] = None) -> None:
         super().__init__(parent)
 
         self.addRoleName(self.NameRole, "name")
         self.addRoleName(self.QualityTypeRole, "quality_type")
+        self.addRoleName(self.LayerHeightRole, "layer_height")
+        self.addRoleName(self.AvailableRole, "available")
 
         self._intent_category = "engineering"
 
         ContainerRegistry.getInstance().containerAdded.connect(self._onChanged)
         ContainerRegistry.getInstance().containerRemoved.connect(self._onChanged)
-
+        self._layer_height_unit = ""  # This is cached
         self._update()
 
     intentCategoryChanged = pyqtSignal()
@@ -56,6 +61,41 @@ class IntentModel(ListModel):
         quality_groups = intent_manager.getQualityGroups(global_stack)
 
         for quality_tuple, quality_group in quality_groups.items():
-            new_items.append({"name": quality_group.name, "intent_category": quality_tuple[0], "quality_type": quality_tuple[1]})
+            new_items.append({"name": quality_group.name,
+                              "quality_type": quality_tuple[1],
+                              "layer_height": self._fetchLayerHeight(quality_group),
+                              "available": True
+                              })
 
+        new_items = sorted(new_items, key=lambda x: x["layer_height"])
         self.setItems(new_items)
+
+    #TODO: Copied this from QualityProfilesDropdownMenuModel for the moment. This code duplication should be fixed.
+    def _fetchLayerHeight(self, quality_group) -> float:
+        global_stack = cura.CuraApplication.CuraApplication.getInstance().getMachineManager().activeMachine
+        if not self._layer_height_unit:
+            unit = global_stack.definition.getProperty("layer_height", "unit")
+            if not unit:
+                unit = ""
+            self._layer_height_unit = unit
+
+        default_layer_height = global_stack.definition.getProperty("layer_height", "value")
+
+        # Get layer_height from the quality profile for the GlobalStack
+        if quality_group.node_for_global is None:
+            return float(default_layer_height)
+        container = quality_group.node_for_global.getContainer()
+
+        layer_height = default_layer_height
+        if container and container.hasProperty("layer_height", "value"):
+            layer_height = container.getProperty("layer_height", "value")
+        else:
+            # Look for layer_height in the GlobalStack from material -> definition
+            container = global_stack.definition
+            if container and container.hasProperty("layer_height", "value"):
+                layer_height = container.getProperty("layer_height", "value")
+
+        if isinstance(layer_height, SettingFunction):
+            layer_height = layer_height(global_stack)
+
+        return float(layer_height)
